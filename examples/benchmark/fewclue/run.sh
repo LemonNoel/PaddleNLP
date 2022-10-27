a3ee8135cad36ab457c8f377c8ba2aa4f51d9ef8
Huijuan Wang <wanghuijuan03@baidu.com>
2022-10-27 14:21:58 +0800

    [fewclue] update script

--- examples/benchmark/fewclue/run.sh
+++ examples/benchmark/fewclue/run.sh
@@ -1,51 +1,55 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 task_name=$1
 device=$2
+fake=$3
 
 if [ $task_name == "csl" ]; then
-    prompt="“{'text':'text_a'}”本文的内容{'mask'}{'mask'}“{'text':'text_b'}”"
-    # prompt="{'text':'text_a'}其中{'text':'text_b'}{'mask'}{'mask'}是关键词" # 0.6142
-    max_length=320
+    max_length=512
 elif [ $task_name == "eprstmt" ]; then
-    prompt="“{'text':'text_a'}”这条评论的情感倾向是{'mask'}{'mask'}的。"
     max_length=128
 elif [ $task_name == "csldcp" ]; then
-    prompt="“{'text':'text_a'}”这篇文献的类别是{'mask'}{'mask'}。"
     max_length=256
 elif [ $task_name == "tnews" ]; then
-    prompt="“{'text':'text_a'}”上述新闻选自{'mask'}{'mask'}专栏。"
     max_length=128 #64
 elif [ $task_name == "iflytek" ]; then
-    prompt="“{'text':'text_a'}”因此，应用类别是{'mask'}{'mask'}。"
     max_length=320
 elif [ $task_name == "ocnli" ]; then
-    prompt="“{'text':'text_a'}”和“{'text':'text_b'}”之间的逻辑关系是{'mask'}{'mask'}。" # 0.4167
-    # prompt="“{'text':'text_a'}”{'mask'}{'mask'}，“{'text':'text_b'}”" # 0.7071
     max_length=128
 elif [ $task_name == "bustm" ]; then
-    # prompt="“{'text':'text_a'}”和“{'text':'text_b'}”之间的逻辑关系是{'mask'}{'mask'}。" # 0.7556
-    prompt="“{'text':'text_a'}”和“{'text':'text_b'}”描述的是{'mask'}{'mask'}的事情。" # 0.7624
-    max_length=64
-elif [ $task_name == "chid" ]; then
-    prompt="“{'text':'text_a'}”这句话中成语[{'text':'text_b'}]的理解正确吗？{'mask'}{'mask'}。"
-    #prompt="{'text':'text_a'}{'text':'text_b'}用在这里对吗？{'mask'}{'mask'}。"
-    max_length=256
-elif [ $task_name == "cluewsc" ]; then
-    prompt="“{'text':'text_a'}”{'text':'text_b'}这里代词使用正确吗？{'mask'}{'mask'}"
     max_length=128
+elif [ $task_name == "chid" ]; then
+    max_length=384
+elif [ $task_name == "cluewsc" ]; then
+    max_length=384
+elif [ $task_name == "cluewsc2020" ]; then
+    max_length=384
 elif [ $task_name == "cmnli" ]; then
-    prompt="“{'text':'text_a'}”和“{'text':'text_b'}”之间的逻辑关系是{'mask'}{'mask'}。"
     max_length=128
 fi
 
-out_dir=./ckpt-cl-multi_$task_name
 
-lrs=(5e-6)
-pptlrs=(5e-6) # 3e-5 3e-4)
+lrs=(3e-6)
+augs=(substitute)
+
 
 for lr in ${lrs[@]}
 do
-    for pptlr in ${pptlrs[@]}
+    for aug in ${augs[@]}
     do
+        out_dir=./checkpoints/ckpt-24k-$task_name
         echo " "
         CUDA_VISIBLE_DEVICES=$device python train_single.py \
         --output_dir $out_dir \
@@ -55,34 +59,53 @@ do
         --t_type "auto" \
         --v_type "multi" \
         --learning_rate $lr \
-        --ppt_learning_rate $pptlr \
-        --max_steps 1000 \
+        --ppt_learning_rate $lr \
+        --max_steps 3000 \
         --logging_steps 10 \
+        --load_best_model_at_end \
         --do_train \
         --do_eval \
-        --do_test \
-        --do_label \
-        --do_predict \
         --do_save True \
+        --do_predict \
+        --do_test \
+        --do_label True \
         --disable_tqdm True \
         --eval_steps 100 \
         --save_steps 100 \
         --warmup_ratio 0.01 \
-        --per_device_eval_batch_size 32 \
-        --per_device_train_batch_size 32 \
+        --save_total_limit 1 \
+        --per_device_eval_batch_size 8 \
+        --per_device_train_batch_size 8 \
+        --gradient_accumulation_steps 4 \
         --model_name_or_path ernie-1.0-large-zh-cw \
         --split_id few_all \
         --task_name $task_name \
         --metric_for_best_model accuracy \
-        --load_best_model_at_end \
-        --ckpt_plm "strategy_supervised/model_4600_89.92.pdparams" 
+        --seed 42 \
+        --ckpt_model cmnli_ckpt24k.pdparams # 句间推理
+
+        #--ckpt_model ckpt_36k_wsc20_4h.pdparams # 无监督阅读理解 + 指代
+        #--ckpt_plm "/ssd2/wanghuijuan03/prompt/PaddleNLP/model_zoo/ernie-1.0/checkpoints-mix/model_36000/model_state.pdparams" # 无监督阅读理解
+        #--ckpt_model cmnli_36k_ckpt_22k.pdparams # 无监督阅读理解 + 句间推理
+        #--ckpt_model cmnli_36k_ckpt_357h.pdparams # 无监督阅读理解 + 句间推理
+        #--ckpt_model strategy_supervised/model_4600_89.92.pdparams # 句间推理 + 文本匹配 
+        #--ckpt_model "results/e1cw/cmnli/checkpoint-24000/model_state.pdparams" # 句间推理
+        #--ckpt_model ckpt_36k_wsc20_4h_cmnli_19k.pdparams # 无监督阅读理解 + 指代 + 句间推理
         echo " "
-        rm -rf $out_dir/checkpoint-*
+        #rm -rf $out_dir/checkpoint-*
     done
 done
-#--evaluation_strategy epoch \
-#--save_strategy epoch 
+
 #--ckpt_model None \
-#--freeze_plm \
-#--soft_encoder mlp \
+        --aug_type $aug \ # 数据增强
+        --freeze_plm \
+        --soft_encoder mlp \
+        --evaluation_strategy epoch \
+        --save_strategy epoch 
+       --fake_file $fake \ # 伪标签
+       --use_rdrop True \
+       --alpha_rdrop 1.0 \
+       --soft_encoder lstm \
+       --gradient_accumulation_steps 2 \
+       --dropout 0.3 \
 
